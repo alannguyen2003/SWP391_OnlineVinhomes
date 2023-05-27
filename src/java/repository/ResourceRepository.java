de8276b3d1872a7494c4911e6bd6995f3a673cc0
tanutv123 <taitanvo2309@gmail.com>
2023-05-28 02:42:19 +0700

    Them method cho ResourceRepository

--- src/java/repository/ResourceRepository.java
+++ src/java/repository/ResourceRepository.java
@@ -19,7 +19,7 @@ import java.util.List;
  */
 public class ResourceRepository {
 
-    public List<BlockResourceEntity> getPaginatedBlockResourceList(int page) throws SQLException {
+    public List<BlockResourceEntity> getPaginatedBlockResourceList(int page, int entries) throws SQLException {
         List<BlockResourceEntity> list = new ArrayList<>();
         Connection con = DBConfig.getConnection();
         String query = """
@@ -27,10 +27,11 @@ public class ResourceRepository {
                        from BlockResource as bs 
                         join BlockVin as b on bs.BID = b.BID 
                         join [Resource] as r on bs.RID = r.RID
-                        order by bs.BID offset ? rows fetch next 8 rows only""";
+                        order by bs.BID offset ? rows fetch next ? rows only""";
 
         PreparedStatement stm = con.prepareStatement(query);
         stm.setInt(1, page);
+        stm.setInt(2, entries);
         ResultSet rs = stm.executeQuery();
         while (rs.next()) {
             BlockResourceEntity br = new BlockResourceEntity();
@@ -44,6 +45,83 @@ public class ResourceRepository {
         return list;
     }
 
+    public List<BlockResourceEntity> getPaginatedBlockResourceListByBlockName(int page, int entries, String blockName, boolean isRunOutOfResource) throws SQLException {
+        List<BlockResourceEntity> list = new ArrayList<>();
+        Connection con = DBConfig.getConnection();
+        String query;
+        if(isRunOutOfResource) {
+             query = """
+                       select bs.BID ,b.[name] as blockName, bs.RID,r.[name] as resourceName, bs.quantity 
+                       from BlockResource as bs 
+                        join BlockVin as b on bs.BID = b.BID 
+                        join [Resource] as r on bs.RID = r.RID
+                        where b.[name] = ? and bs.quantity = 0
+                        order by bs.BID offset ? rows fetch next ? rows only""";
+        } else {
+             query = """
+                       select bs.BID ,b.[name] as blockName, bs.RID,r.[name] as resourceName, bs.quantity 
+                       from BlockResource as bs 
+                        join BlockVin as b on bs.BID = b.BID 
+                        join [Resource] as r on bs.RID = r.RID
+                        where b.[name] = ? 
+                        order by bs.BID offset ? rows fetch next ? rows only""";
+        }
+        PreparedStatement stm = con.prepareStatement(query);
+        stm.setInt(2, page);
+        stm.setString(1, blockName);
+        stm.setInt(3, entries);
+        ResultSet rs = stm.executeQuery();
+        while (rs.next()) {
+            BlockResourceEntity br = new BlockResourceEntity();
+            br.setbId(rs.getInt("bid"));
+            br.setrId(rs.getInt("rid"));
+            br.setBlockName(rs.getString("blockName"));
+            br.setResourceName(rs.getString("resourceName"));
+            br.setQuantity(rs.getInt("quantity"));
+            list.add(br);
+        }
+        return list;
+    }
+    
+    public List<BlockResourceEntity> getPaginatedBlockResourceListBySearchedBlockName(int page, int entries, String searchedName, boolean isRunOutOfResource) throws SQLException {
+        List<BlockResourceEntity> list = new ArrayList<>();
+        Connection con = DBConfig.getConnection();
+        String query;
+        if(isRunOutOfResource) {
+            query = """
+                       select bs.BID ,b.[name] as blockName, bs.RID,r.[name] as resourceName, bs.quantity 
+                       from BlockResource as bs 
+                        join BlockVin as b on bs.BID = b.BID 
+                        join [Resource] as r on bs.RID = r.RID
+                        where b.[name] like ? and bs.quantity = 0
+                        order by bs.BID offset ? rows fetch next ? rows only""";
+        } else {
+            query = """
+                       select bs.BID ,b.[name] as blockName, bs.RID,r.[name] as resourceName, bs.quantity 
+                       from BlockResource as bs 
+                        join BlockVin as b on bs.BID = b.BID 
+                        join [Resource] as r on bs.RID = r.RID
+                        where b.[name] like ?
+                        order by bs.BID offset ? rows fetch next ? rows only""";
+        }
+        PreparedStatement stm = con.prepareStatement(query);
+        stm.setInt(2, page);
+        stm.setString(1, "%"+ searchedName + "%");
+        stm.setInt(3, entries);
+        ResultSet rs = stm.executeQuery();
+        while (rs.next()) {
+            BlockResourceEntity br = new BlockResourceEntity();
+            br.setbId(rs.getInt("bid"));
+            br.setrId(rs.getInt("rid"));
+            br.setBlockName(rs.getString("blockName"));
+            br.setResourceName(rs.getString("resourceName"));
+            br.setQuantity(rs.getInt("quantity"));
+            list.add(br);
+        }
+        return list;
+    }
+   
+    
     public boolean updateResource(BlockResourceEntity br) throws SQLException {
         boolean check = false;
         Connection con = DBConfig.getConnection();
@@ -64,6 +142,6 @@ public class ResourceRepository {
 
     public static void main(String[] args) throws SQLException {
         BlockResourceEntity br = new BlockResourceEntity(1, "Toa cc", 17, "cc", 20);
-        System.out.println(new ResourceRepository().updateResource(br));
+        System.out.println(new ResourceRepository().getPaginatedBlockResourceListBySearchedBlockName(0, 10, "S102", true).size());
     }
 }
