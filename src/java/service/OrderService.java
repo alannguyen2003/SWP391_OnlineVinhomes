c8364233566a88dfc39b8ab9f811240ec6f63753
tanutv123 <taitanvo2309@gmail.com>
2023-07-11 22:51:57 +0700

    Fix update Order notification

--- src/java/service/OrderService.java
+++ src/java/service/OrderService.java
@@ -109,6 +109,10 @@ public class OrderService {
         orderRepository.addOrder(user, cart, note);
     }
     
+    public int getPendingOrders(int blockId) throws SQLException {
+        return orderRepository.getPendingOrders(blockId);
+    }
+    
     public ArrayList<OrderHeaderRequest> getAllOrders() throws Exception {
         ArrayList<OrderHeaderRequest> list = orderRepository.getAllOrders();
         ArrayList<EmployeeOrderRequest> listEmployee = orderRepository.getEmployeeListForOrderList();
